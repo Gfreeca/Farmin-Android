0e147955e35a47a734c874546aeb2a6b594ed4b1
khs3994 <z01039948003@gmail.com>
2023-06-19 15:07:17 +0900

    :sparkles: :: add icon

---
+++ app/src/main/java/com/gfreeca/farmin_android/design_system/FarminIcon.kt
@@ -0,0 +1,29 @@
+package com.gfreeca.farmin_android.design_system
+
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.gfreeca.farmin_android.R
+
+@Composable
+fun FraminLogoIcon(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_farmin_logo),
+        contentDescription = "Farmin Logo Icon",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ProfileIcon(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_profile),
+        contentDescription = "Profile Icon",
+        modifier = modifier
+    )
+}
