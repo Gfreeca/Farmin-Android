7b731d51188efb2e900d3f48ec27ecd85429ec49
khs3994 <z01039948003@gmail.com>
2023-06-26 20:17:14 +0900

    :sparkles: :: bottom navigation 변경

--- app/src/main/java/com/gfreeca/farmin_android/presentation/ui/root/MainActivity.kt
+++ app/src/main/java/com/gfreeca/farmin_android/presentation/ui/root/MainActivity.kt
@@ -19,6 +19,7 @@ import com.gfreeca.farmin_android.presentation.ui.main.component.Screen
 import com.gfreeca.farmin_android.presentation.ui.main.screen.MainScreen
 import com.gfreeca.farmin_android.presentation.ui.main.screen.RecruitScreen
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
